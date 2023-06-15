2d7fcaebf1762765adfd0df606b96793a5c76048
Sanaullah <webdevfarsi@gmail.com>
2023-06-15 21:23:07 +0430

    build

--- index.js
+++ index.js
@@ -3,13 +3,15 @@ import dotenv from 'dotenv';
 import { userMessagerouter } from './routes/getUserMessageRoute.js';
 import cors from 'cors';
 import path from 'path';
+import morgan from 'morgan';
 
 const __dirname = path.resolve();
-
+console.log("dirname is:",path.join(__dirname,"./naw_profile/build"))
 const app = express();
 app.use(express.json())
 dotenv.config();
 app.use(cors())
+app.use(morgan('dev'))
 
 app.use(express.static(path.join(__dirname,"./new_profile/build")));
 
